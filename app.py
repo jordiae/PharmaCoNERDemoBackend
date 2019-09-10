ac972544c0229dd474bf28cdbf28e84895ee45e3
Jordi Armengol Estapé <me@jordiae.com>
2019-09-10 12:43:05 +0200

    Cleaned the code (still wip)

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    """Submit form with text to tag"""
     result = request.form
     return Response(run_pharmaconer(result['Text']), mimetype='text/utf-8')
 
 
 @app.route('/')
 def form():
+    """Home page, form"""
     return render_template('form.html')
 
 
